c88c2bb5836e0c91ca89d4912c71973bf36d62a1
Kumar Giri <rajkumar@juno.com>
2023-02-09 15:53:05 -0500

    changes

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -7,7 +7,7 @@ def main():
 
     """variables and their default values"""
     done = False
-    miles_travelled = 0
+    miles_traveled = 0
     thirst = 0
     distance_from_natives = -30
     water_swigs_left = 3
@@ -21,19 +21,19 @@ def main():
             distance_from_natives +=10
         elif user_choice.upper() == 'B':
             random_numbera = random.randint(20, 40)
-            miles_travelled += random_numbera
+            miles_traveled += random_numbera
             thirst += 15
             distance_from_natives -= random_numbera
         elif user_choice.upper() == 'C':
             random_numberb = random.randint(5, 15)
-            miles_travelled += random_numberb
+            miles_traveled += random_numberb
             thirst += 10
             distance_from_natives += random_numberb
         elif user_choice.upper() == 'D':
             thirst -=20
             distance_from_natives -=15
         elif user_choice.upper() == 'E':
-            print(f'You have travelled {miles_travelled} miles, \nDr. Gunna E\'tcha is {distance_from_natives} behind you, \nYou have {water_swigs_left} number of pills, \nYour tiredness level is {thirst}%')
+            print(f'You have traveled {miles_traveled} miles, \nDr. Gunna E\'tcha is {distance_from_natives} behind you, \nYou have {water_swigs_left} number of pills, \nYour tiredness level is {thirst}%')
             distance_from_natives += 5
         elif user_choice.upper() == 'Q':
             print("You quit.")
@@ -46,11 +46,11 @@ def main():
             print(f"Your hand fatigue is {thirst}%. \nYou're hands gave out. \nNext time monitor your hand tiredness and REMEMBER to take energy pills")
             done = True
         if distance_from_natives >= 0:
-            print("GAME OVER!! Dr.Gunna E'tcha has captured you.")
+            print("GAME OVER!! The natives have captured you.")
             done = True
-        if miles_travelled >=200:
-            print ("CONGRATULATIONS!! You have escpaed from the hands of Evil.")
+        if miles_traveled >=200:
+            print ("CONGRATULATIONS!! You have escaped from the hands of Evil.")
             done = True
 
-# call the main function 
+# call the main function
 main()
