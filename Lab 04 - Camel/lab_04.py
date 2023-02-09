0f99caa5c106698a91e39be8df2a6841e9ed8d36
Raj Kumar Giri <rajkumar@juno.com>
2023-02-08 22:11:38 -0500

    further progress.

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -1,5 +1,5 @@
 import random
-
+# at step no. 13. 
 # main loop
 def main():
     """Game instructions"""
@@ -9,38 +9,46 @@ def main():
     done = False
     miles_travelled = 0
     thirst = 0
+    camel_tiredness = 0
     distance_from_natives = -30
-    water_swigs_left = 3
+    drinks_in_canteen = 3
     while not done:
         print("\nA. Drink from your canteen. \nB. Ahead moderate speed. \nC. Ahead full speed. \nD. Stop for the night. \nE. Status check \nQ. Quit.")
         # ask for user choice
         user_choice = input("\nWhat would you like to do? ")
         if user_choice.upper() == "A":
             thirst -= 20
-            water_swigs_left -=1
+            drinks_in_canteen -=1
             distance_from_natives +=10
         elif user_choice.upper() == 'B':
             random_numbera = random.randint(20, 40)
-            miles_travelled += random_numbera
+            # 10 is the constant pace of the natives
+            miles_travelled += random_numbera + 10
             thirst += 15
-            distance_from_natives -= random_numbera
+            camel_tiredness += 20
+            # 10 is the constant pace of the natives
+            distance_from_natives -= random_numbera + 10
         elif user_choice.upper() == 'C':
             random_numberb = random.randint(5, 15)
             miles_travelled += random_numberb
             thirst += 10
-            distance_from_natives += random_numberb
+            camel_tiredness += 30
+            distance_from_natives = random_numberb
         elif user_choice.upper() == 'D':
             thirst -=20
+            camel_tiredness -= 30
             distance_from_natives -=15
         elif user_choice.upper() == 'E':
-            print(f'You have travelled {miles_travelled} miles, \nDr. Gunna E\'tcha is {distance_from_natives} behind you, \nYou have {water_swigs_left} number of pills, \nYour tiredness level is {thirst}%')
+            print(f'You have travelled {miles_travelled} miles. \nDr. Gunna E\'tcha is {distance_from_natives} behind you. \nYou have {drinks_in_canteen} number of pills. \nCamel_tiredness is {camel_tiredness}%. \nYour tiredness level is {thirst}%.')
             distance_from_natives += 5
         elif user_choice.upper() == 'Q':
-            print("You quit.")
+            print(f"You quit. \nMiles travelled: {miles_travelled} \nDrinks in canteen: {drinks_in_canteen} \nThe natives are {-distance_from_natives} miles behind you.")
             done = True
-        if water_swigs_left == 0:
+        if drinks_in_canteen == 0:
             print("GAME OVER!! You are out of energy pills")
             done=True
+        if distance_from_natives >= -15:
+            print("The natives are getting close.")
 
         if thirst >= 100:
             print(f"Your hand fatigue is {thirst}%. \nYou're hands gave out. \nNext time monitor your hand tiredness and REMEMBER to take energy pills")
